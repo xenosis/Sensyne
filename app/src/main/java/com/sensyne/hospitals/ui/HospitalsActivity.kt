0b740bfbb56124286de37247d1f71aac878d0e74
Paul Teale <pteale@xenosis.mobi>
2020-05-31 11:49:57 +0100

    Adding initial UI layout

---
+++ app/src/main/java/com/sensyne/hospitals/ui/HospitalsActivity.kt
@@ -0,0 +1,13 @@
+package com.sensyne.hospitals.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sensyne.hospitals.R
+
+class HospitalsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
